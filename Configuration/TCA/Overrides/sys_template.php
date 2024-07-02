72b421b4b0617a363925a863323b50c0fae7826e
Sven Wappler <wappler@wappler.systems>
2024-07-02 10:27:09 +0200

    updates

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,36 +1,38 @@
 <?php
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+ExtensionManagementUtility::addStaticFile(
    'ce_timeline',
    'Configuration/TypoScript',
    'Timeline - Content Element'
 );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+ExtensionManagementUtility::addStaticFile(
    'ce_timeline',
    'Configuration/TypoScript/Styles/Default',
    'Timeline - Style: Default'
 );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+ExtensionManagementUtility::addStaticFile(
    'ce_timeline',
    'Configuration/TypoScript/Styles/Darkmode',
    'Timeline - Style: Darkmode'
 );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+ExtensionManagementUtility::addStaticFile(
    'ce_timeline',
    'Configuration/TypoScript/Styles/Lightmode',
    'Timeline - Style: Lightmode'
 );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+ExtensionManagementUtility::addStaticFile(
    'ce_timeline',
    'Configuration/TypoScript/Styles/Pink',
    'Timeline - Style: Pink Borders'
 );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+ExtensionManagementUtility::addStaticFile(
    'ce_timeline',
    'Configuration/TypoScript/Lightbox',
    'Timeline - JavaScript: Lightbox'
